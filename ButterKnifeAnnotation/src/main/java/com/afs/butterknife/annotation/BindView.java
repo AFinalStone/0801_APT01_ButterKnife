f2cb35ff8cd1b70eec8993893122adaae6acc31d
shiyaolei <shiyaolei@yongrun.com>
2022-05-10 19:03:55 +0800

    添加butterknife相关注解

--- ButterKnifeAnnotation/src/main/java/com/afs/butterknife/annotation/BindView.java
+++ ButterKnifeAnnotation/src/main/java/com/afs/butterknife/annotation/BindView.java
@@ -1,13 +1,12 @@
-package com.afs.applike.annotation;
+package com.afs.butterknife.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//注解类，只能用来对类进行注解
 @Retention(RetentionPolicy.CLASS)
-@Target(ElementType.TYPE)
-public @interface AppLikeLifeCycle {
+@Target(ElementType.FIELD)
+public @interface BindView {
+    int value();
 }
-
--- app/src/main/java/com/afs/main/MainActivity.java
+++ app/src/main/java/com/afs/main/MainActivity.java
@@ -1,16 +1,24 @@
 package com.afs.main;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.afs.app.R;
+import com.afs.butterknife.annotation.BindView;
+import com.afs.utils.BindViewTools;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_click)
+    Button mBtnClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BindViewTools.bind(this);
+        mBtnClick.setText("按钮绑定成功");
     }
 }
